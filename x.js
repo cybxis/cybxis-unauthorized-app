e7deeb58b2a8386c407aa6766563eb0abe809d8d
cybxis <63922811+cybxis@users.noreply.github.com>
2021-10-29 23:58:57 +0800

    Update x.js

--- x.js
+++ x.js
@@ -1,13 +1,1 @@
-function go(){
-var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-		alert(this.responseText);
-    }
-  };
-  xhttp.open("GET", "https://www.spotify.com/api/account-settings/v1/profile");
-  xhttp.setRequestHeader("Content-Type","application/json");
-  xhttp.setRequestHeader("X-Requested-With","XMLHttpRequest");
-  xhttp.AllowCredentials=true;
-  xhttp.send();	
-}
+alert(1);
